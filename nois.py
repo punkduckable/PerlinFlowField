2d7bbdf64bccf6e27485f28810ec520f1ffaf13e
Robert Stephany <robert.stephany@utexas.edu>
2021-01-04 14:01:26 -0500

    Changed a variable names, added comments, ;
    I changed the variable k to "num_freq", since this name better reflects
    what the variable actually does. I also added a big comment in the run
    function that explains (loosely) what the fractal function is doing, and
    what n/num_freq do in it.
    
    I also added a bunch of semi-colons throughout the set of code that
    I am maintaining (everything other than the Perlin noise stuff). I
    prefer the code this way.

--- nois.py
+++ nois.py
@@ -1,5 +1,5 @@
 # Dependencies:
-# Run with PYTHON 3 (not python 2)
+# Run with PYTHON 3
 # Requires matplotlib version 1.4 or above
 
 import numpy as np
@@ -22,14 +22,14 @@ class Particles():
         self.max_vel = max_vel;                             # maximum allowed particle velocity
 
         # Initialize pos_x, pos_y using a uniform random distribution
-        self.pos_x = np.random.uniform(0, size - 1, num_particles)
-        self.pos_y = np.random.uniform(0, size - 1, num_particles)
+        self.pos_x = np.random.uniform(0, size - 1, num_particles);
+        self.pos_y = np.random.uniform(0, size - 1, num_particles);
 
         # Initialize particle velocity, acceleration
-        self.vel_x = np.zeros(num_particles)
-        self.vel_y = np.zeros(num_particles)
-        self.acc_x = np.zeros(num_particles)
-        self.acc_y = np.zeros(num_particles)
+        self.vel_x = np.zeros(num_particles);
+        self.vel_y = np.zeros(num_particles);
+        self.acc_x = np.zeros(num_particles);
+        self.acc_y = np.zeros(num_particles);
 
 
 
@@ -50,8 +50,8 @@ class Particles():
                     fx,                                    # (float array) [size x size]
                     fy):                                   # (float array) [size x size]
 
-        self.acc_x = fx
-        self.acc_y = fy
+        self.acc_x = fx;
+        self.acc_y = fy;
 
 
 
@@ -102,11 +102,11 @@ class Particles():
         index_y = np.floor(self.pos_y).astype(int);
 
         # Now determine the force at each position.
-        fx = vector_x[index_x, index_y]
-        fy = vector_y[index_x, index_y]
+        fx = vector_x[index_x, index_y];
+        fy = vector_y[index_x, index_y];
 
         # Return the forces
-        return fx, fy
+        return fx, fy;
 
 
 
@@ -215,21 +215,18 @@ def perlin(n,m):
     return data
 
 
-def fractal(n,k):
+def fractal(n, num_freq):
      """
-     n>k (ints)
+     n> num_freq (ints)
      super imposes different frequency perlin noise
 
      """
      maximum = 2**n
      data = np.zeros((maximum, maximum))
-     for i in range(k):
-         temp = perlin(maximum, 2**(n-i))*(2**(-(1 + i)))
+     for i in range(num_freq):
+         temp = perlin(maximum, 2**(n-i))*(2**(-(1 + i))) # Scale set to 2^i + 1 = 2^n / 2^(n-i) + 1
          data = data + temp
-         #plot(n,data/k,i)
-     data = data/k
-     #data = 1-np.abs(data)
-     #plot(n,data)
+     data = data/num_freq
      return data
 
 
@@ -245,18 +242,18 @@ def plot_flow(x_hist,                  # (float array)
               image_name):             # (string)
 
     # Set up plot.
-    plt.style.use("default")
-    plt.axis('off')
-    plt.figure(1, figsize = (18, 12), dpi = 1200)
+    plt.style.use("default");
+    plt.axis('off');
+    plt.figure(1, figsize = (18, 12), dpi = 1200);
 
     # This specifies the set of possible colors for the plot.
-    c_list = ['#04577A','#53C8FB','#07B1FA','#28627A','#068CC7','#ffffff']                    # Shades of Blue
+    c_list = ['#04577A','#53C8FB','#07B1FA','#28627A','#068CC7','#ffffff'];                    # Shades of Blue
     #c_list = ['#3fe835', "#177511", "#ffffff"];              # Shades of Green
     #c_list = ["#ff0000", "#ff5353", "#ff9797", "#ffebeb", "#bb0101", "#7e0202", "#000000"];   # Shades of Red
-    #c_list = ["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"]              # Cyan, Brown, Black
+    #c_list = ["#AD450C","#FF813D","#FA6E23","#00ADA7","#23FAF2","white","black"];             # Cyan, Brown, Black
 
     # Set of possible particle sizes
-    s_list = [5,6,6,7]
+    s_list = [5,6,6,7];
 
     # Now, make the plot!
     for i in range(0, num_particles):
@@ -271,11 +268,11 @@ def plot_flow(x_hist,                  # (float array)
                     edgecolor = 'None');
 
     # Save flow plot!
-    plt.savefig(image_name, dpi = 900)
+    plt.savefig(image_name, dpi = 900);
     #plt.savefig(image_name + ".svg", format = 'svg')
 
     # Display flow plot.
-    plt.show()
+    plt.show();
 
 
 
@@ -284,14 +281,14 @@ def plot_vectors(vector_x,             # (float array)
                  vector_y,             # (float array)
                  size):                # (int)
     # Set up plot.
-    plt.style.use('default')
-    plt.axis('off')
-    plt.figure(2, figsize = (18, 12))
+    plt.style.use('default');
+    plt.axis('off');
+    plt.figure(2, figsize = (18, 12));
 
     # Set up x, y coordinates in the plot.
-    x = np.linspace(0, size - 1, size)
-    y = np.linspace(0, size - 1, size)
-    x_coords, y_coords = np.meshgrid(x, y)
+    x = np.linspace(0, size - 1, size);
+    y = np.linspace(0, size - 1, size);
+    x_coords, y_coords = np.meshgrid(x, y);
 
     # Plot a "quiver" at each point. The x, y components of this
     # vector are the corresponding components of vector_x, vector_y.
@@ -299,7 +296,7 @@ def plot_vectors(vector_x,             # (float array)
                y_coords,
                vector_x,
                vector_y,
-               color = "black")
+               color = "black");
 
 
 
@@ -312,7 +309,7 @@ def rotate(vector_x,                         # (float)
            angles):                    # (float)
     new_xs = np.cos(angles) * vector_x - np.sin(angles) * vector_y;
     new_ys = np.sin(angles) * vector_x + np.cos(angles) * vector_y;
-    return new_xs, new_ys
+    return new_xs, new_ys;
 
 
 
@@ -324,51 +321,77 @@ def vector_field(data,                 # (float array)
     # Initialize vector_x, vector_y arrays. At first, all vectors point in the y direction.
     # They are then rotated by an amount specified by the "Angles" variable (which is
     # determined by the perlin noise) to get the final vector field.
-    size = len(data[0])
-    vector_x = np.ones((size,size))
-    vector_y = np.zeros((size,size))
+    size = len(data[0]);
+    vector_x = np.ones((size,size));
+    vector_y = np.zeros((size,size));
 
     # get angles at each point. The values of the angle are based on the data from the perlin noise.
     angles = 2*np.pi*data*angle_scale;
 
     # Set force field using initalized vectors and angles
-    vector_x,vector_y = rotate(vector_x, vector_y, angles)
+    vector_x,vector_y = rotate(vector_x, vector_y, angles);
 
     # Scale x, y components of the vector field
-    vector_x = vector_x*x_scale
-    vector_y = vector_y*y_scale
+    vector_x = vector_x*x_scale;
+    vector_y = vector_y*y_scale;
 
     # Plot the vector field
-    plot_vectors(vector_x, vector_y, size)
+    plot_vectors(vector_x, vector_y, size);
 
-    return vector_x,vector_y
+    return vector_x,vector_y;
 
 
 
 # Run function!
-def run(n,                        # Controls size of grid + perlin noise       (int)
-        k,                        # Number of noise layers we average          (int)
+def run(n,                        # The grid has 2^n points                    (int)
+        num_freq,                 # Number of noise frequencies we average     (int)
         num_particles,            # Number of particles                        (int)
         num_updates,              # Number of particle position updates        (int)
         max_vel,                  # Maximum allowed particle velocity          (float)
         angle_scale,              # Scales rotation by noise of force field    (float)
         x_scale = 50,             # Scales x component of force field          (float)
-        y_scale = 200,             # Scales y component of force field         (float)
+        y_scale = 100,             # Scales y component of force field          (float)
         image_name = "myimage"):  # Name of the final image (an svg); saved in the current working directory (stirng)
-    # Assumption: k < n. We need this for the perlin noise to work. 
+    # Assumption: num_freq < n. We need this for the perlin noise to work.
 
     # First, generate the data.
-    data = fractal(n,k)
+    # The data is a 2^n by 2^n grid of floats. Each point hold a float.
+    # To get these floats, we "average" together k "frequencies" of perlin
+    # noise.
+    # The lowest (0th) frequency has 1 a random vector in each corner of the
+    # grid. The value of the noise of this layer is given by interpolating these
+    # vectors.
+    #
+    # The next frequency splits the gird into quarters, and assigns a vector to
+    # each corner of each quater. To get the noise value at each grid point, we
+    # first determine which quarter that point is in. We then interpolate the
+    # 4 corner vectors for that quarter. This interpolated value is the noise
+    # for the grid point.
+    #
+    # In general, the kth frequency partitions each side of the grid into 2^k
+    # equally spaced pieces, and then partitions the grid into 2^k*2^k pieces
+    # using these side partitions. It assigns a random vector to each corner
+    # of these pieces. To get the noise at a grid point, we first determine
+    # which piece of the grid partition the grid point lives in. We the
+    # interpolate the corner vectors for that piece.
+    #
+    # In this way, there are more random vectors in higher frequencies, which
+    # leads to more chaotic noise values.
+    print("Setting up Noise...                ", end = '');
+    data = fractal(n, num_freq);
+    print("Done!\n", end = '');
 
     # Get the size of the data
-    size = len(data[0])
+    size = len(data[0]);
 
     # Set up force field using the data. force_x, force_y represent the x and y
     # components of the force, respectively.
+    print("Setting up Force Field...          ", end = '');
     force_x, force_y = vector_field(data,
                                     angle_scale,
                                     x_scale,
-                                    y_scale)
+                                    y_scale);
+    print("Done!\n", end = '');
 
     # Set up an array of particles
     particles = Particles(size = size,
@@ -377,24 +400,28 @@ def run(n,                        # Controls size of grid + perlin noise       (
 
     # Move the particles through the force field (defined by force_x, force_y)
     # x_hist and y_hist store the tracks of each particles.
+    print("Simulating Particle Movement...    ", end = '');
     x_hist, y_hist = particles.drive(force_x,
                                      force_y,
-                                     num_updates)
+                                     num_updates);
+    print("Done!\n", end = '');
+
 
     # plot the particle paths!
+    print("Generating Flow Plot...            ", end = '');
     plot_flow(x_hist,
               y_hist,
               num_particles,
               num_updates,
               image_name);
-
+    print("Done!\n", end = '');
 
 
 ###############################################################################
 run(n = 7,
-    k = 3,
-    num_particles = 500,
-    num_updates = 500,
+    num_freq = 4,
+    num_particles = 1000,
+    num_updates = 1000,
     max_vel = .2,
-    angle_scale = 1,
+    angle_scale = 3,
     image_name = "wavy");
